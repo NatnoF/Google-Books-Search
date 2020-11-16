9f855b35510eccef1e28da4f2f309f3cffb0e575
Nicolas Fontan <nico.fontan1@gmail.com>
2020-11-15 20:22:10 -0500

    Finished Footer component and linked font awesome

---
+++ client/src/components/Footer/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer>
+      <div className="container text-left">
+      <i class="fab fa-github"> Proudly built using React.js</i>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
