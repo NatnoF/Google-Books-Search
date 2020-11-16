4d0a037826469e2eb0aced344981542d7036aad2
Nicolas Fontan <nico.fontan1@gmail.com>
2020-11-15 19:29:01 -0500

    Finished card component

---
+++ client/src/components/Card/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Card({title, icon, children}) {
+  return (
+    <div className="card">
+      <h5 className="card-header">{icon} {title}</h5>
+      <div className="card-body">
+        {children}
+      </div>
+    </div>
+  );
+}
+
+export default Card;
